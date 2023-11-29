73827f232391402acbab55bd102a7bbbe74c7fb4
Ricardo <101123336+Ricardoh12@users.noreply.github.com>
2023-11-29 08:08:24 -0300

    Delete processamento_portas.ino

--- processamento_portas.ino
+++
@@ -1,7 +0,0 @@
-void processarDadosJSON() {
-  if (mensagem == barsCode1) {
-    digitalWrite(PIN_ESPECIFICO_PARA_BARSCODE1, HIGH);
-  } else if (mensagem == barsCode2) {
-    digitalWrite(PIN_ESPECIFICO_PARA_BARSCODE2, HIGH);
-  }
-}
